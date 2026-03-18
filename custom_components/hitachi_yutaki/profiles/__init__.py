70fc931499a5e43a7fba4ba1ae6d681b90914533
Kouvaris Theofanis <faniskou@gmail.com>
2026-03-18 22:17:32 +0200

    Add YutakiSCombi20162020Profile to profiles

--- custom_components/hitachi_yutaki/profiles/__init__.py
+++ custom_components/hitachi_yutaki/profiles/__init__.py
@@ -8,6 +8,7 @@ from .yutaki_m import YutakiMProfile
 from .yutaki_s import YutakiSProfile
 from .yutaki_s80 import YutakiS80Profile
 from .yutaki_s_combi import YutakiSCombiProfile
+from .yutaki_s_combi_2016_2020 import YutakiSCombi20162020Profile
 from .yutaki_sc_lite import YutakiScLiteProfile
 from .yutampo_r32 import YutampoR32Profile
 
@@ -16,6 +17,7 @@ _LOGGER = logging.getLogger(__name__)
 PROFILES: dict[str, type[HitachiHeatPumpProfile]] = {
     "yutaki_s": YutakiSProfile,
     "yutaki_s_combi": YutakiSCombiProfile,
+    "yutaki_s_combi_2016_2020": YutakiSCombi20162020Profile,
     "yutaki_s80": YutakiS80Profile,
     "yutaki_m": YutakiMProfile,
     "yutampo_r32": YutampoR32Profile,
